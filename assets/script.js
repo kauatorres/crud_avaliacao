abda5a40f8554eaa026a22fc43a0c07d8003872a
Kauã Torres <kaua.3010@gmail.com>
2023-10-04 06:03:35 -0300

    Form

--- assets/script.js
+++ assets/script.js
@@ -25,7 +25,8 @@ $(document).ready(function () {
             success: function (data) {
                 if (data.status == 1) {
                     alert(data.message);
-                    location.reload();
+                    listarTabela();
+                    $("#formContas").trigger("reset");
                 } else {
                     alert(data.message);
                 }
@@ -76,67 +77,6 @@ $(document).ready(function () {
         }
     });
 
-
-    function listarTabela() {
-        $.ajax({
-            url: "api/listagem/list_contas.php",
-            type: "GET",
-            dataType: "json",
-            data: {
-                empresa: $("#filtroEmpresa").val(),
-                valor: $("#filtroValor").val(),
-                data: $("#filtroData").val(),
-                condicao: $("#filtroCondicao").val()
-            },
-            success: function (data) {
-                $("#tabelaContas").empty();
-
-                var html = "";
-                for (var i = 0; i < data.length; i++) {
-                    for (var i = 0; i < data.length; i++) {
-                        html += "<tr>";
-                        html += "<td>" + data[i].nome_empresa + "</td>";
-                        html += "<td>" + data[i].data_pagar + "</td>";
-
-                        //Formata o valor para R$ xx,xx
-                        var valorFormatado = data[i].valor.toLocaleString('pt-br', {
-                            minimumFractionDigits: 2,
-                            maximumFractionDigits: 2
-                        });
-
-                        var valorFormatado_pago = data[i].valor_pago.toLocaleString('pt-br', {
-                            minimumFractionDigits: 2,
-                            maximumFractionDigits: 2,
-                        });
-
-                        //Verifica se a conta está paga ou não
-                        botao = "";
-                        if (data[i].pago == 1) {
-                            data[i].pago = "Pago";
-                        } else {
-                            data[i].pago = "Pendente";
-                            botao = "<button class='btn btn-success mr-2' onclick='marcarContaComoPaga(" + data[i].id_conta_pagar + ")'>Marcar como Pago</button>";
-                        }
-
-                        html += "<td>R$ " + valorFormatado.replace('.', ',') + "</td>";
-                        html += "<td>" + (valorFormatado_pago ? "R$ " + valorFormatado_pago.replace('.', ',') : "nulo") + "</td>";
-                        html += "<td>" + data[i].pago + "</td>";
-                        html += "<td class='text-center'>";
-                        html += "<button class='btn btn-danger mr-2' onclick='deleteConta(" + data[i].id_conta_pagar + ")'>Excluir</button>";
-                        html += "<button class='btn btn-primary mr-2' onclick='editConta(" + data[i].id_conta_pagar + ")'>Editar</button>";
-                        html += botao;
-                        html += "</td>";
-
-
-                        html += "</tr>";
-                    }
-                }
-
-                $("#tabelaContas").append(html);
-            }
-        });
-    }
-
     $("#aplicarFiltros").click(function () {
         listarTabela();
     });
@@ -154,6 +94,67 @@ $(document).ready(function () {
 });
 
 
+function listarTabela() {
+    $.ajax({
+        url: "api/listagem/list_contas.php",
+        type: "GET",
+        dataType: "json",
+        data: {
+            empresa: $("#filtroEmpresa").val(),
+            valor: $("#filtroValor").val(),
+            data: $("#filtroData").val(),
+            condicao: $("#filtroCondicao").val()
+        },
+        success: function (data) {
+            $("#tabelaContas").empty();
+
+            var html = "";
+            for (var i = 0; i < data.length; i++) {
+                for (var i = 0; i < data.length; i++) {
+                    html += "<tr>";
+                    html += "<td>" + data[i].nome_empresa + "</td>";
+                    html += "<td>" + data[i].data_pagar + "</td>";
+
+                    //Formata o valor para R$ xx,xx
+                    var valorFormatado = data[i].valor.toLocaleString('pt-br', {
+                        minimumFractionDigits: 2,
+                        maximumFractionDigits: 2
+                    });
+
+                    var valorFormatado_pago = data[i].valor_pago.toLocaleString('pt-br', {
+                        minimumFractionDigits: 2,
+                        maximumFractionDigits: 2,
+                    });
+
+                    //Verifica se a conta está paga ou não
+                    botao = "";
+                    if (data[i].pago == 1) {
+                        data[i].pago = "Pago";
+                    } else {
+                        data[i].pago = "Pendente";
+                        botao = "<button class='btn btn-success mr-2' onclick='marcarContaComoPaga(" + data[i].id_conta_pagar + ")'>Marcar como Pago</button>";
+                    }
+
+                    html += "<td>R$ " + valorFormatado.replace('.', ',') + "</td>";
+                    html += "<td>" + (valorFormatado_pago ? "R$ " + valorFormatado_pago.replace('.', ',') : "nulo") + "</td>";
+                    html += "<td>" + data[i].pago + "</td>";
+                    html += "<td class='text-center'>";
+                    html += "<button class='btn btn-danger mr-2' onclick='deleteConta(" + data[i].id_conta_pagar + ")'>Excluir</button>";
+                    html += "<button class='btn btn-primary mr-2' onclick='editConta(" + data[i].id_conta_pagar + ")'>Editar</button>";
+                    html += botao;
+                    html += "</td>";
+
+
+                    html += "</tr>";
+                }
+            }
+
+            $("#tabelaContas").append(html);
+        }
+    });
+}
+
+
 function deleteConta(contaId) {
     if (confirm("Deseja realmente excluir essa conta?")) {
         $.ajax({
@@ -167,7 +168,7 @@ function deleteConta(contaId) {
                 console.log(contaId);
                 if (data.status == 1) {
                     alert(data.message);
-                    location.reload();
+                    listarTabela();
                 } else {
                     alert(data.message);
                 }
@@ -193,7 +194,7 @@ function marcarContaComoPaga(contaId) {
             success: function (data) {
                 if (data.status == 1) {
                     alert(data.message);
-                    location.reload();
+                    listarTabela();
                 } else {
                     alert(data.message);
                 }
